15d2ab8a68a2fd3e241c7d7d1ae9cf1a79c3e8f5
Your Name01~ <you@example.com>
2023-06-21 17:45:38 +0500

    test

---
+++ script.js
@@ -0,0 +1,15 @@
+var userName = prompt ('введите свое имя')
+console.log(userName);
+var age = prompt ('введите свой возраст')
+console.log(age);
+var pr1 = +prompt ('1+1=?')
+console.log('Пример 1: 1 + 1 = ' + (1 + 1) + "(Ваш ответ " + pr1 +")");
+var pr2 = +prompt ('1-1=?')
+console.log('Пример 2: 1 - 1 = ' + (1 - 1) + "(Ваш ответ " + pr2 + ")");
+var pr3 = +prompt ('1*1=?')
+console.log('Пример 3: 1 * 1 = ' + (1 * 1) + "(Ваш ответ " + pr3 + ")");
+var pr4 = +prompt ('1/1=?')
+console.log('Пример 4: 1 / 1 = ' + (1 / 1) + "(Ваш ответ " + pr4 + ")");
+var pr5 = +prompt ('123%111=?')
+console.log('Пример 5: 123 % 111 = ' + (123 % 111) + "(Ваш ответ " + pr5 + ")");
+alert('Зайдите в консоль и посмотрите свои ответы')
